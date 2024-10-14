0c72c4bd53ace8c6298d9c821f4202bc81995f37
thonzyy <tuna5127@icloud.com>
2024-10-14 19:52:08 +0900

    문제3 - 같은 단어가 나타난 수 **문제 설명** 각각의 단어가 나타난 수를 출력해라.

---
+++ src/collection/map/test/WordFrequencyTest1.java
@@ -0,0 +1,22 @@
+package collection.map.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordFrequencyTest1
+{
+    public static void main(String[] args)
+    {
+        String text = "orange banana apple apple banana apple";
+
+        //코드 작성
+        Map<String, Integer> map = new HashMap<>();
+        String[] fruits = text.split(" ");
+
+        for (String fruit : fruits)
+            map.put(fruit, map.getOrDefault(fruit, 0) + 1);
+
+        System.out.println(map);
+
+    }
+}
