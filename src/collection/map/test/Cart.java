588253658287f0faf2baeea0d4be518da78b0097
thonzyy <tuna5127@icloud.com>
2024-10-14 20:24:34 +0900

    Cart getOrDefault 이용 add 메소드 수정, 삭제 수량 고려한 remove 메소드 수정

--- src/collection/map/test/Cart.java
+++ src/collection/map/test/Cart.java
@@ -5,26 +5,39 @@ import java.util.Map;
 
 public class Cart
 {
-    Map<Product, Integer> map = new HashMap<>();
+    Map<Product, Integer> cartMap = new HashMap<>();
 
     public void add(Product product, int quantity)
     {//이름과 가격이 같으면 같은 상품
-        if(!map.containsKey(product))
+        int existQuantity = cartMap.getOrDefault(product, 0);
+        cartMap.put(product, existQuantity + quantity);
+      /*  if(!map.containsKey(product))
             map.put(product, quantity);
         else
-            map.put(product, map.get(product) + quantity);
+            map.put(product, map.get(product) + quantity);*/
     }
 
     public void minus(Product product, int quantity)
     {
-        if(map.containsKey(product))
-            map.remove(product, quantity);
+        int existingQuantity = cartMap.getOrDefault(product, 0);
+        int newQuantity = existingQuantity - quantity;
+        if (newQuantity <= 0)
+        {
+            cartMap.remove(product);
+        }
+        else
+        {
+            cartMap.put(product, newQuantity);
+        }
+      /*  if(cartMap.containsKey(product))
+            cartMap.remove(product);*/
+
     }
 
     public void printAll()
     {
         System.out.println("==모든 상품 출력==");
-        for (Map.Entry<Product, Integer> entry : map.entrySet())
-            System.out.println("상품: " + entry.getKey()+" 수량: "+entry.getValue());
+        for (Map.Entry<Product, Integer> entry : cartMap.entrySet())
+            System.out.println("상품: " + entry.getKey() + " 수량: " + entry.getValue());
     }
 }
--- src/collection/map/test/CartMain.java
+++ src/collection/map/test/CartMain.java
@@ -5,7 +5,7 @@ public class CartMain
     public static void main(String[] args)
     {
         Cart cart = new Cart();
-        cart.add(new Product("사과", 1000), 1);
+        cart.add(new Product("사과", 1000), 3);
         cart.add(new Product("바나나", 500), 1);
         cart.printAll();
 
