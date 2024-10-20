15c481f5e355f9ae66c74b0447c00cbfa31e8665
thonzyy <tuna5127@icloud.com>
2024-10-20 12:31:40 +0900

    컬렉션 유틸 편리한 컬렉션 생성
     `List.of(...)` : 를 사용하면 컬렉션을 편리하게 생성할 수 있다. 단 이때는 가변이 아니라 불변 컬렉션이 생성
    된다.
    `List` , `Set` , `Map` 모두 `of()` 메서드를 지원한다.
    불변 컬렉션은 변경할 수 없다. 변경 메서드를 호출하면 `UnsupportedOperationException` 예외가 발생 한다.

---
+++ src/collection/utils/OfMain.java
@@ -0,0 +1,25 @@
+package collection.utils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class OfMain
+{
+    public static void main(String[] args)
+    {
+        //편리한 불변 컬렉션
+        List<Integer> list = List.of(1, 2, 3);
+        //list.add(4);
+        Set<Integer> set = Set.of(1, 2, 3);
+        Map<Integer, String> map = Map.of(1, "one", 2, "two", 3, "three");
+
+        System.out.println("list: " + list);
+        System.out.println("set: " + set);
+        System.out.println("map: " + map);
+        System.out.println("list.getClass(): " + list.getClass());
+        System.out.println("set.getClass(): " + set.getClass());
+        System.out.println("map.getClass(): " + map.getClass());
+
+    }
+}
